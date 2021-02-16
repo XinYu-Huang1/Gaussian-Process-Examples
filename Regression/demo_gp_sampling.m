c6dfc7702c9efd72f7bbd5e59c58014189d50dc4
Max <linzhidi@foxmail.com>
2021-02-16 08:56:10 +0800

    Update demo_gp_sampling.m

--- Regression/demo_gp_sampling.m
+++ Regression/demo_gp_sampling.m
@@ -1,42 +1,59 @@
 % Demo for drawing samples from a Gaussian Process with RBF
-% K = gamma * exp(- |x1-x2|/(2*l^2))
+% 
+% K = gamma * exp(- |x1-x2|/(l^2))
 % Copyright Zhidi Lin 
 
-clc;clear;
+clc; clear; close all
 
-l = 5;                                            % lengthscale 
-gamma = 2;                                        %  gamma = 1
-x = linspace(-10,10,2000)';                       % points where the function will be evaluated 
-mean = zeros(size(x));                            % Mean of GP (0 here)
+% lengthscale 
+l = 2.5;                                         
 
-[covd1,covd2] = meshgrid(x,x);                    % Generate all the possible pairs of points
-cov = gamma * exp(-(covd1 - covd2).^2 ./ l.^2);   % compute the covariance function 
-L = chol(cov + 1e-10 * eye(size(cov)),'lower');   % Cholesky decomposition of covariance matrix
+% gamma = 1
+gamma = 1;                                    
 
-N = 5;                                             % # of samples want to draw
-C = linspecer(N);                                  % set color
+% points where the function will be evaluated 
+x = linspace(-10,10,100)';                        
 
-% plot confidence zone
+% Mean of GP (0 here)
+m = zeros(size(x));                           
+
+% Generate all the possible pairs of points
+[covd1,covd2] = meshgrid(x,x);                   
+
+% compute the covariance function 
+cov = gamma * exp(-(covd1 - covd2).^2 ./ l.^2);   
+
+N = 3;                                             % # of samples want to draw
+% C = linspecer(N);                                % set color
+% C = ["-ok","-sk","-^k"];
+C = ["-.k","-.k","-.k", "-.k", "-.k"];
+% mc = {'w','k',[90 90 90]/255};
+
+%% plot confidence zone
 figure;
 s1 = diag(cov);
-f = [mean+3*sqrt(s1); flip(mean-3*sqrt(s1),1)]; 
-fill([x; flip(x,1)], f, [7 7 7]/8); hold on;
+f = [m+2*sqrt(s1); flip(m-2*sqrt(s1),1)]; 
+h0 = fill([x; flip(x,1)], f, [7 7 7]/8); hold on;
+
+% Cholesky decomposition of covariance matrix
+K = chol(cov + 1e-12 * eye(size(cov)), 'lower');  
 
 for ii = 1:N
-    uids = randn(size(mean));                     %  generate independent random numbers from standard normal distribution.
-    f = mean +  L * uids;                           %  sample from existing
-    plot(x,f,'color',C(ii,:),'linewidth',3); hold on;
-   % scatter(x,f,[],C(ii,:),'filled');hold on;
+    %  generate independent random numbers from standard normal distribution.
+    uids = randn(size(m));                   
+    
+    %  sample from existing
+    f = m +  K * uids;        
+    
+    h(ii) = plot(x,f, C(ii),'linewidth',4, 'MarkerSize', 10, 'MarkerFaceColor', 'k'); hold on;
     if ii == N
         hold off;
     end
 end
-axis([-10 10 -6 6])
-grid on
-xlabel('input, x')
-ylabel('output, fx')
-%title(strcat(['Sampling from prior ', 'l = '],num2str(l)));
-set(gca,'fontsize',28,'linewidth',2);
-legend('95% CR','samples1','samples2','samples3','samples4','samples5','samples6')
-hh = findobj('tag','legend');%|
-set(hh,'fontsize',15) %| ÉèÖÃlegend×ÖºÅ´óÐ¡
+
+axis([-10 10 -3 3])
+xlabel('x')
+ylabel('f(x)')
+legend1 = legend([h(1), h0], 'samples', '95% CR'); %,'sample 2','sample 3');
+set(gca,'FontSize',45)
+grid on;
