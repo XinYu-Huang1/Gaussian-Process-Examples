fd359a2aef75292137a18c03705b079eddcaa997
Max <linzhidi@foxmail.com>
2021-02-16 08:55:10 +0800

    Update demo_gp_prediction_noise.m

--- Regression/demo_gp_prediction_noise.m
+++ Regression/demo_gp_prediction_noise.m
@@ -4,64 +4,72 @@
 % Copyright Zhidi Lin 
 
 clc;clear;
+% close all;
 
-l = 1.5;
-sig_f = 1;
+l = 2.50;
+gamma = 1;
 
-sig_n = 0.1;
-
-x_test = (-10:0.05:10)';
-m_f = zeros(size(x_test));
+sig_n = 0;
 
 % 4 observations (training points)
-x_train = [-3 -2 2 7];
-y_train = [1 0 2 -2];
+x_train = [-6 -2 2 5];
+y_train = [1 0 2 -1];
+
+% test data
+x_test = linspace(-10,10,100)';
 
 % Calculate the partitions of the joint covariance matrix
-[covXXInd1,covXXInd2] = meshgrid(x_train,x_train);
-[covXXsInd1,covXXsInd2] = meshgrid(x_train,x_test);
-[covXsXsInd1,covXsXsInd2] = meshgrid(x_test,x_test);
-K_test = sig_f * exp(-(covXsXsInd1-covXsXsInd2).^2 ./ l.^2);
-K_cross = sig_f * exp(-(covXXsInd1-covXXsInd2).^2 ./ l.^2);
-K_train = sig_f * exp(-(covXXInd1-covXXInd2).^2 ./ l.^2);
-C = K_train + sig_n^2 *eye(size(K_train));
+[XX1,XX2] = meshgrid(x_train,x_train);
+[XXs1,XXs2] = meshgrid(x_train,x_test);
+[XsXs1,XsXs2] = meshgrid(x_test,x_test);
+
+K_XsXs = gamma * exp(-(XsXs1-XsXs2).^2 ./ l.^2);
+K_XXs = gamma * exp(-(XXs1-XXs2).^2 ./ l.^2);
+K_XX = gamma * exp(-(XX1-XX2).^2 ./ l.^2);
+K_XX_noisy = K_XX + sig_n^2 *eye(size(K_XX));
 
 
-L = chol(C);
+chol_KXX_noisy = chol(K_XX_noisy);
 %posterior_mean = covXXs/covXX_noisy * fx_train';
-posterior_mean = (K_cross/L)/L' * y_train';
+
+posterior_mean = (K_XXs/chol_KXX_noisy)/chol_KXX_noisy' * y_train';
 %posterior_cov = covXsXs - covXXs/covXX_noisy * covXXs';
-posterior_cov = K_test - (K_cross/L)/L' * K_cross';
+
+posterior_cov = K_XsXs - (K_XXs/chol_KXX_noisy)/chol_KXX_noisy' * K_XXs';
 
 s2 = diag(posterior_cov);
 f = [posterior_mean+2*sqrt(s2); flip(posterior_mean-2*sqrt(s2),1)];
 
 % plot 
 figure;
-fill([x_test; flip(x_test,1)], f, [7 7 7]/8);
+h11 = fill([x_test; flip(x_test,1)], f, [7 7 7]/8);
 hold on; 
-plot(x_train, y_train, 'r+', 'MarkerSize', 12); hold on;  plot(x_train, y_train, 'ro', 'MarkerSize', 12);hold on;
-h1 = plot(x_test, posterior_mean,'k-', 'LineWidth', 2); 
-axis([-10 10 -6 6])
+h0 = plot(x_train, y_train, 'ok', 'linewidth',4, 'MarkerSize', 20, 'MarkerFaceColor', 'w');hold on;
+% h0 = plot(x_train, y_train, 'xk', 'linewidth',3, 'MarkerSize', 20, 'MarkerFaceColor', 'k'); hold on;  
+h1 = plot(x_test, posterior_mean,'k-', 'LineWidth', 4); 
+axis([-10 10 -3 3])
 grid on
-xlabel('input, x')
-ylabel('output, fx')
+xlabel('x')
+ylabel('f(x)')
 hold on;
 
 % sampling from posterior 
-K = chol(posterior_cov + 1e-10 * eye(size(posterior_cov)),'lower');   % Cholesky decomposition of covariance matrix
+K = chol(posterior_cov + 1e-12 * eye(size(posterior_cov)),'lower');   % Cholesky decomposition of covariance matrix
 
-N = 2;                                                                % # of samples want to draw
-C = linspecer(N);                                                     % set color
-
+N = 3;                                                                % # of samples want to draw
+% C = linspecer(N);                                                     % set color
+C = ["-.k","-.k","-.k"];
+% C = ["-ok","-sk","-^k"];
 for ii = 1:N
     uids = randn(size(posterior_mean));                                %  generate independent random numbers from standard normal distribution.
-    f = posterior_mean' + transpose(uids) * K;                                     %  sample from existing
-    h(ii) = plot(x_test',f,'color',C(ii,:),'linewidth',3); hold on;
+    f = posterior_mean + K * uids;                                     %  sample from existing
+%     h(ii) = plot(x_test',f,'--','color',C(ii,:),'linewidth',1); hold on;
+    h(ii) = plot(x_test, f, C(ii),'linewidth',3, 'MarkerSize', 10, 'MarkerFaceColor', 'w'); hold on;
 %     scatter(x_test',f,[],C(ii,:),'filled');hold on;
     if ii == N
         hold off;
     end
 end
-legend([h1,h],'mean function','smaple1','sample2')
-title(strcat([' Prediction with noisy observations ', 'l = '],num2str(l)))
+legend1 = legend([h0,h1,h(1),h11],'observations','mean function','samples','95% CR');
+set(gca,'FontSize',45)
+% title(strcat([' Prediction with noisy observations ', 'l = '],num2str(l)))
